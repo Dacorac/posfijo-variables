a43fb7419f8ff96f754d1d3f65b7e250f81a9b19
Daniela Córdoba <dacorac43@gmail.com>
2017-09-03 18:19:31 -0500

    Procesamiento de una ecuación representada en Pos Orden

---
+++ Nodo.py
@@ -0,0 +1,37 @@
+class Nodo():
+	
+	def __init__(self, valor, izq = None, der = None):
+		self.valor = valor
+		self.izq = izq
+		self.der = der
+
+class Tabla():
+	variables = {}
+
+def evaluar(arbol):
+	if arbol.valor == '+':
+		return int(evaluar(arbol.izq)) + int(evaluar(arbol.der))
+
+	if arbol.valor == '-':
+		return int(evaluar(arbol.izq)) - int(evaluar(arbol.der))
+	
+	if arbol.valor == '/':
+		return int(evaluar(arbol.izq)) / int(evaluar(arbol.der))
+
+	if arbol.valor == '*':
+		return int(evaluar(arbol.izq)) * int(evaluar(arbol.der))
+
+	if arbol.valor == '=':
+		valor = evaluar(arbol.izq)
+		Tabla.variables[arbol.der] = valor
+		return valor
+	
+	if Tabla.variables.has_key(arbol.valor) == True:
+		return Tabla.variables[arbol.valor]
+	elif Tabla.variables.has_key(arbol.valor) == False:
+		print "devolviendo valor"
+		return int(arbol.valor)
+			
+			
+
+
---
+++ PosOrden.py
@@ -0,0 +1,33 @@
+from Nodo import *
+from Pila import Pila
+
+def leerCadena():
+	cadena = raw_input('Ingrese la cadena de caracteres a operar:')
+	#listaCaracteres = []
+	listaCaracteres = cadena.split(' ')
+	return listaCaracteres
+
+def crearArbol(lista, pila):
+        for i in range(0,len(lista)):
+                if lista[i] in "+-*/=":
+                        der = pila.sacarElemento()
+                        izq = pila.sacarElemento()
+                        nodoAux = Nodo(lista[i], izq, der)
+                        pila.agregarElemento(nodoAux)
+                else:
+                        pila.agregarElemento(Nodo(lista[i], None, None))
+        return pila.sacarElemento()
+
+def imprimirArbolPostfijo(arbol):
+        if arbol != None:
+                imprimirArbolPostfijo(arbol.izq)
+                imprimirArbolPostfijo(arbol.der)
+                print arbol.valor               
+
+
+cadena = leerCadena()
+pila = Pila()
+nodo = crearArbol(cadena, pila)
+print "impresion de Arbol:"
+imprimirArbolPostfijo(nodo)
+print "Resultado operacion: " , evaluar(nodo)
